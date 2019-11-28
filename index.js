49f97f9cda79b5fcccc0b61954874ec0e5b29902
fajariya3210 <angkasarayanet@gmail.com>
2019-11-28 14:12:02 +0900

    update2

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const puppeteer = require('puppeteer');
 
+
 (async()=>{
     const browser = await puppeteer.launch({headless:false});
     const page = await browser.newPage();
-    await page.goto("https://www.udemy.com/");
-    await page.screenshot({path:"hasil.png"});
+    await page.goto("http://nursajaya.com/", 
+            {waitUntil: ['load', 'domcontentloaded']});
+    await page.type('#s' , "tas ultah");
+    await page.keyboard.press("Enter", 
+            {waitUntil: ['load', 'domcontentloaded']})
+    await page.screenshot({path:"screenshot1.png" , fullPage:true ,
+            waitUntil: ['load' ,'domcontentloaded' ]})
     await browser.close()
     
 })();
